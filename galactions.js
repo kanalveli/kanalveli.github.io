0c23810942e7eccf5359791809c33aa0f8ce3833
kanal <kanalveli@freshdesk.com>
2015-06-22 19:12:57 +0530

    instagram connect

--- galactions.js
+++ galactions.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-	$("#instaimgs").fetchUserPhotos();
+
+	$("#instaimgs").fetchUserImg({
+		clientID:'3d4a6c6f983c463e9f0d688bdf892609',
+			username:'kanalveli'
+		});
+
 
 });
--- pluginGal.js
+++ pluginGal.js
@@ -1,47 +1,56 @@
-(function($){
+;(function($) {
 
-	$.fn.fetchUserPhotos=function(options){
-		var defaults={
-			clientID:'3d4a6c6f983c463e9f0d688bdf892609',
-			usrname:'kanalveli',
-			maxphotos:10
+    $.fn.fetchUserImg= function(options) {
+		if(!this.length) { 
+			return this; 
+		}
 
-		};
-		var settings={};
-		settings=$.extend(defaults, options);
-
-		$.ajax({
-			type ='GET',
-			url  ='https://api.instagram.com/v1/users/search?q='+settings.usrname+'&client_id='+settings.clientID,
-			dataType='jsonp',
-			success: function(data){
-				alert("success");
-				var imghtml='<li><img src="'+data.data[0].profile_picture+'"></li>';
-				$(this).prepend(imghtml);
-				$.ajax({
-					type ="GET",
-					url  ='https://api.instagram.com/v1/users/'+data.data[0].id+'/media/recent/?client_id='+settings.clientID+'&count='+settings.maxphotos,
-					dataType='jsonp',
-					success: function(data){
-						if(data.meta.code==200 && data.data.length){
-							for(var i=0;i<data.data.length;i++){
-								var img1=data.data[i];
-								if(img1.type==='image'){
-									var imghtml='<li><img src="'+img1.standard_resolution.url+'"></li>';
-									$(this).prepend(imghtml);
-								}
+        var defaults = {
+            username: null,
+            clientID: null,
+            limit: null,
+            loadMore: null,
+            error: function() {},
+            success: function() {}
+        }
+        plugin = this;
+		plugin.settings = {}
+ 		plugin.settings = $.extend({}, defaults, options);
+ 		el = $(this);
+        loadContent();
+    },
+    
+    loadContent = function(){
+        el.each(function() {
+	        $.ajax({
+		       	type: 'GET',
+		       	url: 'https://api.instagram.com/v1/users/search?q='+plugin.settings.username+'&client_id='+plugin.settings.clientID+'&callback=?',
+		       	dataType: 'jsonp',
+		       	success: function(data) {
+			        var thisUser = data.data[0];
+			        if(thisUser.username === plugin.settings.username) {
+						var url = 'https://api.instagram.com/v1/users/'+thisUser.id+'/media/recent/?client_id='+plugin.settings.clientID+'&count='+plugin.settings.limit+'&callback=?';
+			        	$.ajax({
+						    type: 'GET',
+						    url: url,
+					       	dataType: 'jsonp',
+					       	success: function(data) {
+						       	if(data.meta.code === 200 && data.data.length) {
+						        	for(var i = 0; i < data.data.length; i++) {
+						        		var thisMedia = data.data[i],
+						        			item;
+						        		if(thisMedia.type === 'image') {
+							       			item = '<img class="instaphotos" src="'+thisMedia.images.standard_resolution.url+'" alt="Instagram Image" data-filter="'+thisMedia.filter+'" />';
+								       	}
+							       		el.append(item);
+							       	}
+							       		
+						       	} 	
 							}
-						}
-
-					}
- 
-				});
-			},
-			error: function(){
-				alert("error");
-			}
-		});
-
-	}
-
-}(jquery));
+						});	
+				    }
+		        }
+	        });
+	    });
+    }
+})(jQuery);
